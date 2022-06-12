42e0c0c73897f5feaf6a4fada737da635139605d
junya <zunya1130@yahoo.co.jp>
2022-06-13 00:15:28 +0900

    mibaeyoku

--- src/main/java/imix/imix/PrimaryController.java
+++ src/main/java/imix/imix/PrimaryController.java
@@ -8,17 +8,28 @@ import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.WritablePixelFormat;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 public class PrimaryController {
 	@FXML private ImageView imageT;
 	@FXML private ImageView imageB;
 	@FXML private ImageView mixedImage;
+	@FXML private StackPane imageTopPane;
+	@FXML private Text textImageTop;
+	@FXML private StackPane imageBottomPane;
+	@FXML private Text textImageBottom;
+	@FXML private StackPane imageMixedPane;
+	@FXML private Text textImageMixed;
+	@FXML private TextField threshold;
+	@FXML private TextField checkRowCount;
 	public String ImageName = null;
     
     @FXML
@@ -32,6 +43,8 @@ public class PrimaryController {
 	        Image img = new Image("file:" + file.getPath());
 	        this.ImageName = file.getPath();
 	        this.imageT.setImage(img);
+	        this.imageTopPane.setBackground(null);
+	        this.textImageTop.setText(null);
     	}
     }
     
@@ -45,6 +58,8 @@ public class PrimaryController {
 	    	System.out.println(file.getPath());
 	        Image img = new Image("file:" + file.getPath());
 	        this.imageB.setImage(img);
+	        this.imageBottomPane.setBackground(null);
+	        this.textImageBottom.setText(null);
     	}
     }
     
@@ -88,7 +103,8 @@ public class PrimaryController {
         int samePixels = 0;
         boolean topRow = true;
         int topSameRows = 2;
-        double threshold = 0.8;
+        double threshold = Double.parseDouble(this.threshold.getText());
+    	System.out.println("threshold : " + threshold);
 
         // ピクセルmHeight
         for( int y = 0 ; y < bHeight ; y++ ) {
@@ -162,17 +178,10 @@ public class PrimaryController {
 	            int pixel   = bottomPixels[ index ];
 	            bottomAfterPixels[ index - (topSameRows * bWidth) ] = pixel;
 	        }
-	    }
-	    
-	    int minHeight = 0;
-	    if(tAfterHeight <= bAfterHeight) {
-	    	minHeight = tAfterHeight;
-	    } else {
-	    	minHeight = bAfterHeight;
-	    }
-	    
+	    }	    
 
-	    int checkRowCount = 50;
+	    int checkRowCount = Integer.parseInt(this.checkRowCount.getText());
+    	System.out.println("checkRowCount : " + checkRowCount);
         int[]                           checkSameRow  = new int[ bWidth * checkRowCount ];
         // ピクセルmHeight
 	    for( int y = 0 ; y < checkRowCount; y++ ) {
@@ -184,7 +193,6 @@ public class PrimaryController {
 	        }
 	    }
 	    
-    	int shikichi = 150;
     	int maxSamePixels = 0;
     	int maxSameRow = 0;
     	//行で確認
@@ -309,6 +317,8 @@ public class PrimaryController {
 		}
        
        this.mixedImage.setImage(mImg);
+       this.imageMixedPane.setBackground(null);
+       this.textImageMixed.setText(null);
 
     }
 }
